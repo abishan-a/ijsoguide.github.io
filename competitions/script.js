54775bf779ea7e7d0c908f33045e570a20781c35
ignjatovicpavle <pavle.ignjatovic.08+portfolio@gmail.com>
2025-10-29 18:51:17 +0100

    Automating competition link appearance

---
+++ competitions/script.js
@@ -0,0 +1,29 @@
+import {db} from '/centralAuthenticationSystem.js';
+import { doc, getDoc , collection} from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js"
+
+let startDate = new Date('2025-11-01T00:00:00Z')
+let endDate = new Date('2026-11-01T00:00:00Z')
+let current = new Date();
+if (startDate < current && current < endDate){
+    document.getElementById('registrationButton').style.display = "none";
+
+    /** Obtaining the problems link */
+    let problemsLink = "";
+
+    getDoc(doc(db, "otherData", "challenge2025"))
+    .then((data)=> {
+        if(data.exists()){
+            let d = data.data()
+            console.log(d.problemLink)
+            problemsLink = d.problemLink;
+            document.getElementById('problemsLink').innerHTML = "Competition problems";
+            document.getElementById('problemsLink').href = problemsLink;
+            document.getElementById('problemsLink').style.display = "inline-block"
+        } else{
+            console.log("No data.")
+        }
+    })
+    .catch(error => {
+        console.log("Error getting document:", error);
+    });
+}
